2951e5fc0f8bf01ce7f59847a64e9b43bf50c919
Andrew Soltes <soltesandrew@gmail.com>
2020-05-02 14:03:03 -0400

    added the templates for groups and refactors

--- template_nornir.py
+++ template_nornir.py
@@ -8,34 +8,94 @@ try:
 except FileExistsError:
     pass
 
-#store the source csv in variable
-source_file_hosts = "template_source.csv"    
-# store the jinja2 file in a variable
-template_file_hosts = "nornir_hosts.j2" 
+def host(hosts):
+        #store the source csv in variable
+        source_file_hosts = "csv/hosts_template_source.csv"    
+        # store the jinja2 file in a variable
+        template_file_hosts = "templates/nornir_hosts.j2" 
 
-#create an empty string for the ouput container
-host_configs =""   
+        #create an empty string for the ouput container
+        host_configs =""   
 
-#open the template_file and assign to variable f
-with open (template_file_hosts) as f: 
-    #reading jinja2 environment and assigning to var interface template
-    interface_template = Template(f.read(), keep_trailing_newline=True) 
-    
-# opening the csv file and assigning to var f
-with open(source_file_hosts) as f:
-    reader = csv.DictReader(f) #reading the csv file
-    for row in reader:  #loop in all rows of csv file and assigning to row variable
-        host_data = interface_template.render(   #assigned all the rendered file into variables
-            # assigning all the row values into variables                                       
-            hostname = row['hostname'],
-            ipaddress = row['ipaddress'],
-            platform = row['platform'],
-            groups = row['groups'],)
-        host_configs += host_data #appending all the values from hosts_data to output_configs
+        #open the template_file and assign to variable f
+        with open (template_file_hosts) as f: 
+            #reading jinja2 environment and assigning to var interface template
+            interface_template = Template(f.read(), keep_trailing_newline=True) 
+            
+        # opening the csv file and assigning to var f
+        with open(source_file_hosts) as f:
+            reader = csv.DictReader(f) #reading the csv file
+            for row in reader:  #loop in all rows of csv file and assigning to row variable
+                host_data = interface_template.render(   #assigned all the rendered file into variables
+                    # assigning all the row values into variables                                       
+                    hostname = row['hostname'],
+                    ipaddress = row['ipaddress'],
+                    platform = row['platform'],
+                    groups = row['groups'],)
+                host_configs += host_data #appending all the values from hosts_data to output_configs
 
-with open(path + "hosts.yml", 'w') as f:
-    f.write(host_configs)
+        with open(path + "hosts.yml", 'w') as f:
+            f.write(host_configs)
+            return (f)
+        
+
+     
+def group(groups):
+        #store the source csv in variable
+        source_file_hosts = "csv/groups_template_source.csv"    
+        # store the jinja2 file in a variable
+        template_file_hosts = "templates/nornir_groups.j2" 
+
+        #create an empty string for the ouput container
+        groups_config =""   
+
+        #open the template_file and assign to variable f
+        with open (template_file_hosts) as f: 
+            #reading jinja2 environment and assigning to var interface template
+            interface_template = Template(f.read(), keep_trailing_newline=True) 
+            
+        # opening the csv file and assigning to var f
+        with open(source_file_hosts) as f:
+            reader = csv.DictReader(f) #reading the csv file
+            for row in reader:  #loop in all rows of csv file and assigning to row variable
+                host_data = interface_template.render(   #assigned all the rendered file into variables
+                    # assigning all the row values into variables                                       
+                    groups = row['groups'],
+                    platform = row['platform'],
+                    username = row['username'],
+                    password = row['password'],
+                    asn = row['asn'],)
+                groups_config += host_data #appending all the values from hosts_data to output_configs
+
+        with open(path + "groups.yml", 'w') as f:
+            f.write(groups_config)
+            return (f)
+        
 
 
-    
+group(group)
+host(host)   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+host(host)
 
